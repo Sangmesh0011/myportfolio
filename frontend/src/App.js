e3497b0547892ec53a2e64b15dc3b87ec5658b7e
Sangmesh0011 <78312170+Sangmesh0011@users.noreply.github.com>
2024-02-16 14:59:09 +0530

    updated resume

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,7 +40,7 @@ function App() {
             Explore
             <AiFillCaretDown />
           </div>
-          <a href="/SANGMESH J.pdf" download="Sangmesh J.pdf" rel="noopener noreferrer">
+          <a href="/SangmeshJResume.pdf" download="SangmeshJResume.pdf" rel="noopener noreferrer">
             <div className="expbtn">
               Download CV <AiOutlineCloudDownload />
             </div>
