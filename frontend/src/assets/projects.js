f5bc42f478cff1ef3d65c123046d6064615828d8
Sangmesh0011 <78312170+Sangmesh0011@users.noreply.github.com>
2024-03-01 16:26:37 +0530

    minor changes some responsiveness

--- frontend/src/assets/projects.js
+++ frontend/src/assets/projects.js
@@ -3,7 +3,7 @@ const projects = [
     "id": 1,
     "title": "E-Commerce Website - Shoelifestyle",
     "img": "/shoe.jpeg",
-    "img2": "/shoe.jpg",
+    "img2": "/shoe.jpeg",
     "description": "A react based web app to buy shoes.",
     "detail1": "React JS, Express JS, Node JS, Bootstrap, Redux Toolkit, MongoDB, PayPal API",
     "detail2":"Introducing our cutting-edge e-commerce website developed with React JS, Express JS, Node JS, Bootstrap, Redux Toolkit, MongoDB, and PayPal API. Dive into a user-friendly platform where you can explore a diverse product catalog, effortlessly add items to your cart, and experience a smooth checkout process. Our website ensures secure transactions through PayPal API integration, providing a trustworthy environment for online purchases. Stay updated with real-time order tracking, responsive customer support, and a scalable architecture, making your shopping journey convenient and enjoyable.",
